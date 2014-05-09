15de30bda4f031c6c861427dec9dca4f411dabd3
Kazusa Okuda <kazusa@klamath.jp>
2014-05-09 09:21:12 +0900

    initial Commit

---
+++ fm1001.h
@@ -0,0 +1,9 @@
+/*
+ *
+ *  FM Audio Generator Controller  Version 0.1
+ *  (C) 2014 Kazusa Okuda (a.k.a. klaNath)
+ *
+ *  This software is freely distributable under the terms of an MIT-style license.
+ *  For details, see the https://github.com/klaNath/midiRX.git/LICENSE
+ *
+ */
