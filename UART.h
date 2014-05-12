67e22c6fab2a33968b68797235e684e3febf4187
Kazusa Okuda <kazusa@klamath.jp>
2014-05-12 10:50:13 +0900

    Add Lincency to UART Library

---
+++ UART.h
@@ -0,0 +1,18 @@
+/*
+ *
+ *  UART Controller for PIC12F1/16F1   Version 0.1
+ *  (C) 2014 Kazusa Okuda (a.k.a. klaNath)
+ *
+ *  This software is freely distributable under the terms of an MIT-style license.
+ *  For details, see the https://github.com/klaNath/midiRX.git/LICENSE
+ *
+ */
+
+
+/*
+ *
+ *  CAUTION!!!!
+ * This library implemented only reciev function.
+ *
+ */
+
