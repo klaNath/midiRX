d51a5ca2b52a296954b756b80fe273cbccf87398
Kazusa Okuda <kazusa@klamath.jp>
2014-05-13 11:34:16 +0900

    add SPI library

---
+++ SPI.h
@@ -0,0 +1,9 @@
+/*
+ *
+ *  SPI Controller for PIC12F1/16F1   Version 0.1
+ *  (C) 2014 Kazusa Okuda (a.k.a. klaNath)
+ *
+ *  This software is freely distributable under the terms of an MIT-style license.
+ *  For details, see the https://github.com/klaNath/midiRX.git/LICENSE
+ *
+ */
