7a06fcac7653ec6e6432d719ec068f0d5173da2f
Kazusa Okuda <kazusa@klamath.jp>
2014-05-13 10:42:53 +0900

    Implement main Loutin in main.c

--- main.c
+++ main.c
@@ -12,9 +12,39 @@
 #include "MIDIRX.h"
 #include "fm1001.h"
 
-
+void initSys(void);
 
 void interrupt IRQ()
 {
     if(PIE1bits.RCIE == 1) getMIDI();
+}
+
+
+void main(void)
+{
+
+    unsigned char state, note, och;
+
+    initSys();
+    initFM();
+
+    while(1)
+    {
+        if(decodeMIDI() == 1)   state = getStatus();
+
+        if(state == NOTE_ON)
+        {
+            note = getNote();
+            och = getSendCh(0);
+
+            sendFM(note, och, 1);
+        }
+        else if(state == NOTE_OFF)
+        {
+            note = getNote();
+            och = getSendCh(note);
+
+            sendFM(note, och, 0);
+        }
+    }
 }
