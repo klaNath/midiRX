887f52da24bc4cafd7952b12f6a417319024acbd
Kazusa Okuda <kazusa@klamath.jp>
2014-05-27 10:13:44 +0900

    add debug header and macro

--- main.c
+++ main.c
@@ -12,6 +12,10 @@
 #include "MIDIRX.h"
 #include "fm1001.h"
 
+#define SYS_FREQ 32000000
+
+#define __DEBUG
+
 __CONFIG(CLKOUTEN_OFF & FOSC_INTOSC & FCMEN_OFF & IESO_OFF & BOREN_ON & PWRTE_ON
 & WDTE_OFF & MCLRE_OFF & CP_OFF & CPD_OFF) ;
 __CONFIG(PLLEN_ON & STVREN_ON & WRT_OFF & BORV_HI & LVP_OFF);
@@ -36,7 +40,6 @@ void main(void)
     unsigned char state, note, och;
 
     initSys();
-    initFM();
 
     while(1)
     {
@@ -48,15 +51,20 @@ void main(void)
         {
             note = getNote();
             och = getSendCh(0);
-
+#ifndef __DEBUG
             sendFM(note, och, 1);
+#endif
+            PORTA = 0x04;
+
         }
         else if(state == NOTE_OFF)
         {
             note = getNote();
             och = getSendCh(note);
-
+#ifndef
             sendFM(note, och, 0);
+#endif
+            PORTA = 0;
         }
     }
 }
@@ -68,9 +76,13 @@ void initSys()
     TRISA = 0x38;
     ANSELA = 0;
     INTCON = 0xC0;
+    PORTA = 0;
     //PIE1 = 0x20;                                                  //move to initUART()
-    
+
+    clearMIDI();
     initUART(UART_RX_ONLY);
+#ifndef __DEBUG
     initFM();
+#endif
 }
 
