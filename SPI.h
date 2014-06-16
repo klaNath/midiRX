d211fe25916039becdda2b92fe38de8a19091c71
Kazusa Okuda <klamathcatalyst@gmail.com>
2014-06-16 17:28:01 +0900

    prepare function using for SPI

--- SPI.h
+++ SPI.h
@@ -7,3 +7,25 @@
  *  For details, see the https://github.com/klaNath/midiRX.git/LICENSE
  *
  */
+
+
+
+#ifndef _SPI_H
+#define _SPI_H
+
+enum SPI_INIT_STATE
+{
+	SPI_NONE,
+	SPI_BOTH,
+	SPI_RX_ONLY,
+	SPI_TX_ONLY
+};
+
+void initSPI(enum SPI_INIT_STATE);
+
+unsigned char getSPI();
+unsigned char sendSingleSPI(unsigned char);
+unsigned char sendMultiSPI(unsigned char*, unsigned char);
+
+
+#endif
