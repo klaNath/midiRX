05e885a3441f41202e983bfdf4bafb5c5e4e2aad
Kazusa Okuda <klamathcatalyst@gmail.com>
2014-06-19 11:03:24 +0900

    add comment of function behavior and parameter

--- fm1001.h
+++ fm1001.h
@@ -8,13 +8,57 @@
  *
  */
 
+/*
+ *this library, fm1001 controller manager, needs Low Lavel SPI function.
+ *
+ * enum SPI_INIT_STATE
+ *{
+ *	SPI_NONE,
+ *	SPI_MASTER,
+ *	SPI_SLAVE
+ *};
+ *
+ *void initSPI(enum SPI_INIT_STATE);
+ *unsigned char getSPI();
+ *void sendSingleSPI(unsigned char);
+ *void sendMultiSPI(unsigned char*, unsigned char);
+ *
+ * Their has required to use their function. SPI mode is (0,0) master mode.
+ */
+
+#include "SPI.h"
+
 #ifndef _FM1001_H
 #define _FM1001_H
 
-void initFM(void);
+void initFM(char);
+/**
+ *
+ * @param 0 => normal mode. 1=> fixed velocity mode
+ * @return None
+ */
 
 unsigned char getSendCh(unsigned char);
+/*
+ * Return is FM synthesizer channel to send.
+ * 1st argument is 0,
+ * then this function will return empty or most oldest channnel.
+ * Argument is not 0, first, serching channel has same note number and
+ * returning this channel.
+ * However no correct channel, function return empty or most oldest channel.
+ *
+ */
+
 unsigned char sendFM(unsigned char, unsigned char, unsigned char);
+/*unsigned char sendFM(unsigned char, unsigned char, unsigned char)
+ *
+ * Return is OK or FAIL indicater. OK return 0, FAIL return 0xFF
+ *
+ * 1st argument is FM sinthesizer channel, 0-15
+ * 2nd argument is Note number, 0-127.
+ * 3rd argument is Velocity, OFF is 0, other 1-127 is ON.
+ *
+ */
 
 
 #endif
